f498d894caef3ed78a6072131fc9f05268ce816f
Dapimex <denis-pimenov2011@yandex.ru>
2020-04-22 15:06:49 +0300

    Fix server

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,29 @@
 import React from "react"
 import Layout from "../components/layout"
-import API from "../utils/API"
 
 function getRequest(url) {
-  API.get(url)
-    .then((res) => {result = res})
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', baseUrl + url, true);
+
+  xhr.onload = function() {
+    result = this.responseText
+    alert(this.responseText)
+  }
+
+  xhr.onerror = function() {
+    alert("Something wrong")
+  }
+
+  xhr.send();
 }
 
 const apis = [
   "/register?alias=Dapimex&status=true",
   "/list",
 ]
+
+const baseUrl = "http://52.14.64.130:7000/dns"
 
 let result = "Demo API response"
 
@@ -21,7 +34,7 @@ export default () => (
       return (
         <div>
           <p>{value}</p>
-          <button onClick={getRequest(value)}>Try API</button>
+          <button onClick={() => getRequest(value)}>Try API</button>
         </div>
       )
     })}
